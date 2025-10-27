380cb3d7e3a861aa55a25f960c690b470ebad809
ciscoacdave <30909276+ciscoacdave@users.noreply.github.com>
2025-10-27 16:32:52 -0500

    Component-only branch

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: 'src/web-component.js',
+      name: 'AirtableCRM',
+      fileName: 'airtable-crm-component',
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[ext]',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+      }
+    }
+  }
 })
